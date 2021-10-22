f10fb409d25b8a887a05ecdd0d9840d4b560c007
VanSilva <48038684+VanSilva@users.noreply.github.com>
2021-10-22 20:12:39 -0300

    adicionado app default

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import "react-native-gesture-handler";
+import React from "react";
+import { Routes } from "./src/routes";
+import { AuthProvider } from "./src/contexts/auth";
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
+  );
+}
