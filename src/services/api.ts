886fade15849d617025e7ad34d9668fd3d5e3727
VanSilva <48038684+VanSilva@users.noreply.github.com>
2021-11-29 21:49:47 -0300

    modificado endereco de conexao com o backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.API_URL || 'http://192.168.0.106:3334',
-})
+  baseURL: process.env.API_URL || "https://lar-temp-backend.herokuapp.com/",
+});
