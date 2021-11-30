8f35fb386316a942910a2123422d2fb1f5d9d622
VanSilva <48038684+VanSilva@users.noreply.github.com>
2021-11-29 22:19:59 -0300

    adicionado configurações do App

--- App.tsx
+++ App.tsx
@@ -2,6 +2,10 @@ import "react-native-gesture-handler";
 import React from "react";
 import { Routes } from "./src/routes";
 import { AuthProvider } from "./src/contexts/auth";
+import { LogBox } from "react-native";
+
+LogBox.ignoreLogs(['Remote debugger']);
+
 
 export default function App() {
   return (
