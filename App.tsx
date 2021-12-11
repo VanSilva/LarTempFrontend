7ab82448b0fa7a0eddfe43f2a1f42caad43da39e
VanSilva <48038684+VanSilva@users.noreply.github.com>
2021-12-11 20:47:03 -0300

    adicionado recurso de fontes

--- App.tsx
+++ App.tsx
@@ -4,13 +4,27 @@ import { Routes } from "./src/routes";
 import { AuthProvider } from "./src/contexts/auth";
 import { LogBox } from "react-native";
 
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
 LogBox.ignoreLogs(['Remote debugger']);
 
 
 export default function App() {
-  return (
-    <AuthProvider>
-      <Routes />
-    </AuthProvider>
-  );
+  const [fontsLoaded] = useFonts({
+    'montserrat-extralight': require('./assets/fonts/Montserrat-ExtraLight.ttf'),
+    'montserrat-regular': require('./assets/fonts/Montserrat-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <AppLoading />
+    )
+  } else {
+    return (
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    );
+  }
 }
